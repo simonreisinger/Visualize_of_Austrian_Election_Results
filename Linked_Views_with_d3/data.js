2227e51d6efe9bf86e50aa6ad43ee0724ac7725b
ippon1 <s_reisinger@hotmail.com>
2020-04-02 20:57:03 +0200

    update

--- Linked_Views_with_d3/data.js
+++ Linked_Views_with_d3/data.js
@@ -7,14 +7,87 @@ let colors = {
     "GRÜNE":"#88B626",
     "SONST.":"#222"
 };
+let election_data = null
 
 // In this file, all the data handling should be done, for example:
 // * loading the CSV file
+
 // * computing the overall percentages for entire Austria
 // (check the correctness of your computation here: https://www.bmi.gv.at/412/Nationalratswahlen/Nationalratswahl_2019/ )
-// * synchronization between choropleth map and pie chart
 
-choropleth();
+
+function init_x()
+{
+    let file_location = "https://raw.githubusercontent.com/ippon1/Visualize_of_Austrian_Election_Results/master/Linked_Views_with_d3/NRW2019_Bundeslaender.csv"
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file_location, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                var rows = allText.split("\r\n");
+
+                var electionData = {};
+
+                parties = rows[0].split(',');
+
+                // Sum up total votes per party
+                electionData["total"] = {};
+                electionData["total"]["votes"] = 0;
+                for (var i = 2; i < parties.length; i++) {
+                    electionData["total"][parties[i].toString() + "_abs"] = 0;
+                }
+
+                //move line by line
+                for (var i = 1; i < rows.length; i++) {
+                    //split by separator (,) and get the columns
+                    var cols = rows[i].split(',');
+                    //move column by column
+                    var currentState = "";
+                    for (var j = 0; j < cols.length; j++) {
+                        var value = cols[j];
+                        if (j === 0) // Add New State
+                        {
+                            currentState = value.toString();
+                            electionData[currentState] = {};
+                            //console.log(electionData["total"])
+                        } else {
+                            electionData[currentState][parties[j].toString()] = value;
+                            if (parties[j].toString() !== "votes"){
+                                electionData[currentState][parties[j].toString() + "_abs"] = Math.floor(electionData[currentState]["votes"]* value/100);
+                                electionData["total"][parties[j].toString() + "_abs"] += electionData[currentState][parties[j].toString() + "_abs"];
+                            } else {
+                                electionData["total"]["votes"] += parseInt(value);
+                            }
+                        }
+                    }
+                }
+                for (var i = 2; i < parties.length; i++) {
+                    electionData["total"][parties[i].toString()] = 100 * electionData["total"][parties[i].toString() + "_abs"] / electionData["total"]["votes"];
+                }
+                console.log(electionData);
+                election_data = electionData;
+
+                // * synchronization between choropleth map and pie chart
+                choropleth();
+                pie(election_data);
+
+            }
+        }
+    };
+    rawFile.send(null);
+}
+
+
+init_x();
+
+
+
+
+
 
 
 
--- Linked_Views_with_d3/pie.js
+++ Linked_Views_with_d3/pie.js
@@ -1,1 +1,105 @@
 // D3 pie chart example: https://observablehq.com/@d3/pie-chart
+let width = 150;
+let height = 150;
+let data = null
+
+function pie(electionData) {
+
+    const object = { a: 5, b: 6, c: 7  };
+    const xxx = (({ a, c }) => ({ a, c }))(object);
+    console.log(xxx); // { a: 5, c: 7 }
+
+
+    console.log(electionData)
+    console.log(Object.keys(colors));
+    var parties = Object.assign({},  Object.keys(colors))
+    console.log(parties);
+
+
+
+    data = electionData["total"];
+    console.log(data)
+    const picked = ((parties) => (parties ))(data);
+    console.log(picked)
+
+    console.log("PIE");
+
+    // set the dimensions and margins of the graph
+    var margin = 40;
+
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin;
+
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("#svg_pie")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+// Create dummy data
+    console.log(data);
+    //var data = {ÖVP: 9, SPÖ: 20, FPÖ:30, NEOS:8, JETZT:12}
+
+
+    //const picked = (({ a, c }) => ({ a, c }))(object);
+
+    console.log(data);
+
+// Compute the position of each group on the pie:
+    var pie = d3.pie().value(function(d) {return d.value; });
+    var data_ready = pie(d3.entries(data));
+
+    console.log(data_ready);
+
+
+
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+        svg
+            .selectAll('whatever')
+            .data(data_ready)
+            .enter()
+            .append('path')
+            .attr('d', d3.arc()
+                .innerRadius(0)
+                .outerRadius(radius)
+            )
+            .attr('fill', function(d){ return colors[d.data.key] })
+            .attr("stroke", "black")
+            .style("stroke-width", "2px")
+            .style("opacity", 0.7)
+
+    /*
+
+        const arcs = pie(data);
+
+            let svg = d3.select("#svg_pie")
+                .attr("viewBox", [-width / 2, -height / 2, width, height]);
+
+            svg.append("g")
+                .attr("stroke", "white")
+                .selectAll("path")
+                .data(arcs)
+                .join("path")
+                .attr("fill", d => colors(d.data.name))
+                .attr("d", arc)
+                .append("title")
+                .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
+
+            svg.append("g")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", 12)
+                .attr("text-anchor", "middle")
+                .selectAll("text")
+                .data(arcs)
+                .join("text")
+                //.attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+                .call(text => text.append("tspan")
+                    .attr("y", "-0.4em")
+                    .attr("font-weight", "bold")
+                    .text(d => d.data.name))
+        */
+}
