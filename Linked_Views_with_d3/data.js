2892e3fd357c64e852ba02e2ef0420f2a9fb5134
iPPON <ippon1@riseup.net>
2020-05-28 10:23:00 +0200

    data loading updated

--- Linked_Views_with_d3/data.js
+++ Linked_Views_with_d3/data.js
@@ -18,71 +18,68 @@ let election_data = null;
 
 function init() {
     let file_location = "./NRW2019_Bundeslaender.csv";
-    let rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file_location, true);
-    rawFile.onreadystatechange = function () {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status === 0) {
-                let allText = rawFile.responseText;
-                let rows = allText.split("\r\n");
+    d3.csv(file_location).then(function (data) {
+        console.log(data);
 
-                let electionData = {};
+        let parties = Object.keys(data[0]);
 
-                parties = rows[0].split(',');
+        let electionData = {};
 
-                // Sum up total votes per party
-                electionData["total"] = {};
+        // Sum up total votes per party
+        electionData["total"] = {};
 
-                //electionData["total"]["votes"] = 0;
-                for (let i = 1; i < parties.length; i++) {
-                    electionData["total"][parties[i].toString()] = {
-                        name: parties[i].toString(),
-                        percantage: 100,
-                        abs: 0
-                    };
-                }
-
-                //move line by line
-                for (let i = 1; i < rows.length; i++) {
-                    //split by separator (,) and get the columns
-                    let cols = rows[i].split(',');
-                    //move column by column
-                    let currentState = "";
-                    for (let j = 0; j < cols.length; j++) {
-                        let value = cols[j];
-                        if (j === 0) // Add New State
-                        {
-                            currentState = value.toString();
-                            electionData[currentState] = {};
-                        } else {
-                            if (parties[j].toString() !== "votes") {
-                                electionData[currentState][parties[j].toString()] = {
-                                    name: parties[j].toString(),
-                                    percantage: value,
-                                    abs: Math.floor(electionData[currentState]["votes"].abs * value / 100)
-                                };
-                                electionData["total"][parties[j].toString()].abs += electionData[currentState][parties[j].toString()].abs;
-                            } else {
-                                electionData["total"]["votes"].abs += parseInt(value);
-                                electionData[currentState][parties[j].toString()] = {percantage: 0, abs: value};
-                            }
-
-                        }
-                    }
-                }
-                for (var i = 2; i < parties.length; i++) {
-                    electionData["total"][parties[i].toString()].percantage = 100 * electionData["total"][parties[i].toString()].abs / electionData["total"]["votes"].abs;
-                }
-                election_data = electionData;
-                // * synchronization between choropleth map and pie chart
-                firstPassThePoll("Wien");
-                choropleth(firstPassThePoll());
-                updatePie("total");
-
+        for (let i = 1; i < parties.length; i++) {
+            if (parties[i] !== "Bundesland") {
+            //if (Object.keys(colors).indexOf(parties[i]) > -1) {
+                electionData["total"][parties[i].toString()] = {
+                    name: parties[i].toString(),
+                    percantage: 100,
+                    abs: 0
+                };
             }
         }
-    };
-    rawFile.send(null);
+
+        //move line by line
+        for (let i = 0; i < data.length; i++) {
+            //split by separator (,) and get the columns
+            let cols = data[i];
+            console.log(cols);
+
+            //move column by column
+            let currentState = "";
+            for (let value of Object.keys(cols)) {
+                if (value === "Bundesland") // Add New State
+                {
+                    currentState = cols[value];
+                    electionData[currentState] = {};
+                } else if (value === "votes") {
+                    electionData["total"]["votes"].abs += parseInt(cols[value]);
+                    electionData[currentState][value] = {percantage: 0, abs: cols[value]};
+
+                } else {
+                    electionData[currentState][value] = {
+                        name: value,
+                        percantage: cols[value],
+                        abs: Math.floor(electionData[currentState]["votes"].abs * cols[value] / 100)
+                    };
+                    electionData["total"][value].abs += electionData[currentState][value].abs;
+                }
+            }
+        }
+
+        console.log(electionData);
+
+        for (let value of Object.keys(colors)) {
+        //for (var i = 2; i < Object.keys(colors).length; i++) {
+            electionData["total"][value].percantage = 100 * electionData["total"][value].abs / electionData["total"]["votes"].abs;
+        }
+        election_data = electionData;
+        // * synchronization between choropleth map and pie chart
+        firstPassThePoll("Wien");
+        choropleth(firstPassThePoll());
+        updatePie("total");
+
+    });
 }
 
 function firstPassThePoll() {
